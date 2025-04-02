7f37c71e64595f1b488fc36c434f40a1c9c4cdcf
David Hagerty <david@dathagerty.com>
2025-04-01 21:18:49 -0400

    feat: render actual HTML rather than plain text

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::IntoResponse, routing::get, Router};
+use tracing::{info, instrument};
+use flair::{get_tagline, get_word};
+use maud::{html, DOCTYPE};
+
+mod flair;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let site = Router::new()
+        .route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, site).await.unwrap();
+}
+
+#[instrument]
+async fn root() -> impl IntoResponse {
+    info!("serving");
+
+    html!({
+        (DOCTYPE)
+        a href="/" {
+            h1 {
+                "david's " (get_word())
+            }
+        }
+        h2 {
+            (get_tagline())
+        }
+        "Holy crap, it's a website"
+    })
 }
