b36b9288e7f9ab821446c8bcd42bbba1a6f454df
David Hagerty <david@dathagerty.com>
2025-04-02 20:50:40 -0400

    refactor: use new markup templates

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use axum::{response::IntoResponse, routing::get, Router};
 use tracing::{info, instrument};
-use flair::{get_tagline, get_word};
-use maud::{html, DOCTYPE};
 
 mod flair;
+mod templates;
 
 #[tokio::main]
 async fn main() {
@@ -19,17 +18,5 @@ async fn main() {
 #[instrument]
 async fn root() -> impl IntoResponse {
     info!("serving");
-
-    html!({
-        (DOCTYPE)
-        a href="/" {
-            h1 {
-                "david's " (get_word())
-            }
-        }
-        h2 {
-            (get_tagline())
-        }
-        "Holy crap, it's a website"
-    })
+    templates::index()
 }
