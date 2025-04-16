5b7afac4186d3f4dc500351e384cdee9553ac1ae
David Hagerty <david@dathagerty.com>
2025-04-15 20:29:21 -0400

    feat: build info injection

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 use axum::{response::IntoResponse, routing::get, Router};
 use tracing::{info, instrument};
+use build_info::build_info;
 
 mod flair;
 mod templates;
+
+build_info!(fn version);
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
+    // I know this is a git repository, so let's panic if things don't work for some reason.
+    let vc = version().version_control.as_ref().expect("how is this not a git repository");
+
+    info!(commit = vc.git().unwrap().commit_short_id, "starting website");
     let site = Router::new()
         .route("/", get(root));
 
