5289633dbe4625fccd9f8155c1b4b5c9f976f0c3
David Hagerty <david@dathagerty.com>
2025-04-15 20:43:26 -0400

    feat: added templates

--- src/templates.rs
+++ src/templates.rs
@@ -1,19 +1,62 @@
 use maud::{html, Markup, DOCTYPE};
 use crate::flair::{get_tagline, get_word};
+use tracing::{info, instrument};
 
-fn head() -> Markup {
+const COMMIT_HASH: &str = build_info::format!("{} on {}", $.version_control?.git()?.commit_short_id, $.timestamp);
+
+#[instrument]
+pub fn root() -> Markup {
+    info!("rendering root");
+    html! {
+        (DOCTYPE)
+        (head("home".to_string()))
+        (header())
+        (page_content())
+        (footer())
+    }
+}
+
+#[instrument]
+pub fn page() -> Markup {
+    info!("rendering page");
+    html! {
+        (DOCTYPE)
+        (head("page title".to_string()))
+        (header())
+        (page_content())
+        (footer())
+    }
+}
+
+#[instrument]
+pub fn post(slug: String) -> Markup {
+    info!("rendering post");
+    html! {
+        (DOCTYPE)
+        (head("post title".to_string()))
+        (header())
+        (post_content(slug))
+        (footer())
+    }
+}
+
+#[instrument]
+fn head(title: String) -> Markup {
+    info!("rendering head");
     html! {
         head {
             meta http-equiv="Content-Type" content="text/html;charset=utf-8";
             meta charset="utf-8";
             meta name="viewport" content="width=device-width, initial-scale=1.0";
             meta theme-color="#a3be8c";
-            title { "put a title here" }
+            title { (title) }
         }
     }
 }
 
+#[instrument]
 fn header() -> Markup {
+    info!("rendering header");
     html! {
         header #header {
             a href="/" {
@@ -22,17 +65,49 @@ fn header() -> Markup {
             h2 { (get_tagline()) }
             nav {
                 a href="/about" { "about" }
+                " "
+                a href="/blag" { "blag" }
+                " | "
                 a href="https://hachyderm.io/@dathagerty" rel="me" { "mastodon" }
+                " "
+                a href="https://bsky.app/profile/dathagerty.com" rel="me" { "bsky" }
+                " "
+                a href="https://dathagerty.omg.lol" rel="me" { "omg.lol" }
+                " "
+                a href="https://github.com/dathagerty" rel="me" { "github" }
+                " "
+                a href="https://git.sr.ht/~gloatingfiddle" rel="me" { "sr.ht" }
             }
         }
     }
 }
 
-pub(crate) fn index() -> Markup {
+#[instrument]
+pub fn footer() -> Markup {
+    info!("rendering footer");
     html! {
-        (DOCTYPE)
-        (head())
-        (header())
-        "wow content"
+        footer #footer {
+            "Built with no emotion because it's automated from the code in commit "
+            a href="https://github.com/dathagerty/website" {
+                (COMMIT_HASH)
+            }
+        }
+    }
+}
+
+
+#[instrument]
+pub fn post_content(slug: String) -> Markup {
+    info!(slug, "rendering post content");
+    html! {
+        "wow it's content for " (slug)
+    }
+}
+
+#[instrument]
+pub fn page_content() -> Markup {
+    info!("rendering page content");
+    html! {
+        "wow it's a page"
     }
 }
